f9fdf5f18aa8078a142fd65e196f6937d94c97b8
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-06-07 11:00:47 +0200

    got graphviz

--- Generics/MRSOP/GraphViz.hs
+++ Generics/MRSOP/GraphViz.hs
@@ -18,6 +18,7 @@ import Generics.MRSOP.Base
 import Generics.MRSOP.Util
 import Control.Monad
 
+import Generics.MRSOP.Examples.SimpTH
 
 
 {-showFix
@@ -26,13 +27,15 @@ import Control.Monad
   -> String
 showFix (Fix rep) = elimRep show1 showFix mconcat rep-}
   
-visualizeNA :: Show1 ki => NA ki (Fix ki codes) a -> Dot NodeId
+visualizeNA :: (Show1 ki, HasDatatypeInfo ki fam codes)
+            => Proxy fam -> NA ki (Fix ki codes) a -> Dot NodeId
 -- TODO: This recursive call is problematic, as we have no way of infering HasDatatypeInfo
 -- as we threw away the `fam` in which the `NA` is present
-visualizeNA (NA_I i) = visualizeFix' i
-visualizeNA (NA_K k) = node [("label", show1 k)]
+visualizeNA _ (NA_I i) = visualizeFix i
+visualizeNA _ (NA_K k) = node [("label", show1 k)]
 
 
+{-
 -- | This version does not use HasDatatypeInfo
 visualizeFix' :: forall ki codes ix. (IsNat ix, Show1 ki) => Fix ki codes ix -> Dot NodeId
 visualizeFix' (Fix rep) = 
@@ -42,22 +45,26 @@ visualizeFix' (Fix rep) =
       fields <- elimNPM visualizeNA prod
       traverse (constr .->.) fields
       pure constr
-      
+-} 
 
 
-{-visualizeFix
-  :: forall ki fam codes ix. (Show1 ki, IsNat ix , HasDatatypeInfo ki fam codes ix) 
+visualizeFix
+  :: forall ki fam codes ix. (Show1 ki, IsNat ix , HasDatatypeInfo ki fam codes) 
   => Fix ki codes ix
   -> Dot NodeId
 visualizeFix (Fix rep) =
   case sop rep of
     Tag c prod ->
       let
-        info = datatypeInfo (Proxy :: Proxy fam) (Proxy :: Proxy ix)
+        info = datatypeInfo (Proxy :: Proxy fam) (getSNat (Proxy :: Proxy ix))
         constrInfo = constrInfoLkup c info
       in do
         constr <- node [("label", constructorName constrInfo)]
-        fields <- elimNPM visualizeNA prod 
+        fields <- elimNPM (visualizeNA (Proxy :: Proxy fam)) prod 
         traverse (constr .->.) fields
         pure constr
--}
+
+
+func = deep @FamStmtString (SDecl $ test1 "fib" "n" "aux")
+
+res = visualizeFix func
